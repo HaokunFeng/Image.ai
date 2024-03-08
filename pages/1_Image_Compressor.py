6237d9689362a9252ee5670f0b4defda9b3900e7
Haokun.Feng <haokunf@uw.edu>
2024-03-07 18:13:17 -0800

    update

--- pages/1_Image_Compressor.py
+++ pages/1_Image_Compressor.py
@@ -12,29 +12,25 @@ def compress_image(input_image, quality=85):
 def main():
     st.title("Image Compressor")
 
-    # 上传图像
-    uploaded_file = st.file_uploader("选择要压缩的图像", type=["jpg", "jpeg"])
+    uploaded_file = st.file_uploader("Select your image", type=["jpg", "jpeg"])
 
     if uploaded_file is not None:
         original_image = Image.open(uploaded_file)
-        st.image(original_image, caption="原始图像", use_column_width=True)
+        st.image(original_image, caption="Original Image", use_column_width=True)
 
-        # 获取压缩质量
-        quality = st.slider("选择压缩质量（0-100）", 0, 100, 85)
 
-        if st.button("压缩图像"):
-            # 压缩图像
+        quality = st.slider("Select Compression Quality(0-100)", 0, 100, 85)
+
+        if st.button("Compress Image"):
             compressed_image = compress_image(original_image, quality)
-            st.image(compressed_image, caption="压缩后的图像", use_column_width=True)
+            st.image(compressed_image, caption="Compressed image", use_column_width=True)
 
-            # 显示图像大小
             #original_size_kb = len(uploaded_file.getvalue()) / 1024
             #compressed_size_kb = len(compressed_image.tobytes()) / 1024
-            #st.write(f"原始图像大小: {original_size_kb:.2f} KB")
-            #st.write(f"压缩后的图像大小: {compressed_size_kb:.2f} KB")
+            #st.write(f"Original Size: {original_size_kb:.2f} KB")
+            #st.write(f"Compressed Size: {compressed_size_kb:.2f} KB")
 
 
-            # 提供下载按钮
             compressed_image_bytes = io.BytesIO()
             compressed_image.save(compressed_image_bytes, format="JPEG")
             compressed_image_bytes = compressed_image_bytes.getvalue()
